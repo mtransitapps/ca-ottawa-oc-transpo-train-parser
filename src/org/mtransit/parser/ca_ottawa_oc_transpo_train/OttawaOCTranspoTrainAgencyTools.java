a16e4fad641438553bddae0989eef812fed881ca
Mathieu Méa <mathieu.mea@gmail.com>
2015-05-25 13:32:33 -0400

    Compatibility with May 19 update.

--- src/org/mtransit/parser/ca_ottawa_oc_transpo_train/OttawaOCTranspoTrainAgencyTools.java
+++ src/org/mtransit/parser/ca_ottawa_oc_transpo_train/OttawaOCTranspoTrainAgencyTools.java
@@ -159,6 +159,18 @@ public class OttawaOCTranspoTrainAgencyTools extends DefaultAgencyTools {
 		return super.cleanStopName(gStopName);
 	}
 
+	private static final String EE = "EE";
+	private static final String EO = "EO";
+	private static final String NG = "NG";
+	private static final String NO = "NO";
+	private static final String WA = "WA";
+	private static final String WD = "WD";
+	private static final String WH = "WH";
+	private static final String WI = "WI";
+	private static final String WL = "WL";
+	private static final String PLACE = "place";
+	private static final String RZ = "RZ";
+
 	@Override
 	public int getStopId(GStop gStop) {
 		String stopCode = getStopCode(gStop);
@@ -169,26 +181,28 @@ public class OttawaOCTranspoTrainAgencyTools extends DefaultAgencyTools {
 		matcher.find();
 		int digits = Integer.parseInt(matcher.group());
 		int stopId = 0;
-		if (gStop.stop_id.startsWith("EE")) {
+		if (gStop.stop_id.startsWith(EE)) {
 			stopId = 100000;
-		} else if (gStop.stop_id.startsWith("EO")) {
+		} else if (gStop.stop_id.startsWith(EO)) {
 			stopId = 200000;
-		} else if (gStop.stop_id.startsWith("NG")) {
+		} else if (gStop.stop_id.startsWith(NG)) {
 			stopId = 300000;
-		} else if (gStop.stop_id.startsWith("NO")) {
+		} else if (gStop.stop_id.startsWith(NO)) {
 			stopId = 400000;
-		} else if (gStop.stop_id.startsWith("WA")) {
+		} else if (gStop.stop_id.startsWith(WA)) {
 			stopId = 500000;
-		} else if (gStop.stop_id.startsWith("WD")) {
+		} else if (gStop.stop_id.startsWith(WD)) {
 			stopId = 600000;
-		} else if (gStop.stop_id.startsWith("WH")) {
+		} else if (gStop.stop_id.startsWith(WH)) {
 			stopId = 700000;
-		} else if (gStop.stop_id.startsWith("WI")) {
+		} else if (gStop.stop_id.startsWith(WI)) {
 			stopId = 800000;
-		} else if (gStop.stop_id.startsWith("WL")) {
+		} else if (gStop.stop_id.startsWith(WL)) {
 			stopId = 900000;
-		} else if (gStop.stop_id.startsWith("place")) {
+		} else if (gStop.stop_id.startsWith(PLACE)) {
 			stopId = 1000000;
+		} else if (gStop.stop_id.startsWith(RZ)) {
+			stopId = 1100000;
 		} else {
 			System.out.println("Stop doesn't have an ID (start with)! " + gStop);
 			System.exit(-1);
