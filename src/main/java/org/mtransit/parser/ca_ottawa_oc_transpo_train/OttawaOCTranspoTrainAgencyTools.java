8171d2fb5abb15a9726138ec92ac706599ea2d0f
Mathieu Méa <mathieu.mea@gmail.com>
2021-02-22 11:15:37 -0500

    Use OC Transpo provider commons & cleanup

--- src/main/java/org/mtransit/parser/ca_ottawa_oc_transpo_train/OttawaOCTranspoTrainAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_ottawa_oc_transpo_train/OttawaOCTranspoTrainAgencyTools.java
@@ -2,22 +2,16 @@ package org.mtransit.parser.ca_ottawa_oc_transpo_train;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.mtransit.parser.CleanUtils;
+import org.mtransit.commons.CharUtils;
+import org.mtransit.commons.CleanUtils;
+import org.mtransit.commons.StringUtils;
+import org.mtransit.commons.provider.OttawaOCTranspoProviderCommons;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.StringUtils;
-import org.mtransit.parser.Utils;
-import org.mtransit.parser.gtfs.data.GCalendar;
-import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
-import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
-import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MRoute;
-import org.mtransit.parser.mt.data.MTrip;
 
-import java.util.HashSet;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,55 +23,19 @@ import static org.mtransit.parser.StringUtils.EMPTY;
 // https://www.octranspo.com/files/google_transit.zip
 public class OttawaOCTranspoTrainAgencyTools extends DefaultAgencyTools {
 
-	public static void main(@Nullable String[] args) {
-		if (args == null || args.length == 0) {
-			args = new String[3];
-			args[0] = "input/gtfs.zip";
-			args[1] = "../../mtransitapps/ca-ottawa-oc-transpo-train-android/res/raw/";
-			args[2] = ""; // files-prefix
-		}
+	public static void main(@NotNull String[] args) {
 		new OttawaOCTranspoTrainAgencyTools().start(args);
 	}
 
-	@Nullable
-	private HashSet<Integer> serviceIdInts;
-
 	@Override
-	public void start(@NotNull String[] args) {
-		MTLog.log("Generating OC Transpo train data...");
-		long start = System.currentTimeMillis();
-		this.serviceIdInts = extractUsefulServiceIdInts(args, this, true);
-		super.start(args);
-		MTLog.log("Generating OC Transpo train data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	public boolean defaultExcludeEnabled() {
+		return true;
 	}
 
+	@NotNull
 	@Override
-	public boolean excludingAll() {
-		return this.serviceIdInts != null && this.serviceIdInts.isEmpty();
-	}
-
-	@Override
-	public boolean excludeCalendar(@NotNull GCalendar gCalendar) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarInt(gCalendar, this.serviceIdInts);
-		}
-		return super.excludeCalendar(gCalendar);
-	}
-
-	@Override
-	public boolean excludeCalendarDate(@NotNull GCalendarDate gCalendarDates) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarDateInt(gCalendarDates, this.serviceIdInts);
-		}
-		return super.excludeCalendarDate(gCalendarDates);
-	}
-
-	@Override
-	public boolean excludeTrip(@NotNull GTrip gTrip) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessTripInt(gTrip, this.serviceIdInts);
-		}
-		return super.excludeTrip(gTrip);
+	public String getAgencyName() {
+		return "OC Transpo";
 	}
 
 	@NotNull
@@ -146,9 +104,9 @@ public class OttawaOCTranspoTrainAgencyTools extends DefaultAgencyTools {
 	public String getRouteColor(@NotNull GRoute gRoute) {
 		if (StringUtils.isEmpty(gRoute.getRouteColor())) {
 			//noinspection deprecation
-			Matcher matcher = DIGITS.matcher(gRoute.getRouteId());
+			final Matcher matcher = DIGITS.matcher(gRoute.getRouteId());
 			if (matcher.find()) {
-				int routeId = Integer.parseInt(matcher.group());
+				final int routeId = Integer.parseInt(matcher.group());
 				if (routeId == 1L) {
 					return "DA291C";
 				}
@@ -162,14 +120,6 @@ public class OttawaOCTranspoTrainAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
-	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
-		mTrip.setHeadsignString(
-				cleanTripHeadsign(gTrip.getTripHeadsignOrDefault()),
-				gTrip.getDirectionIdOrDefault()
-		);
-	}
-
-	@Override
 	public boolean directionFinderEnabled() {
 		return true;
 	}
@@ -177,17 +127,8 @@ public class OttawaOCTranspoTrainAgencyTools extends DefaultAgencyTools {
 	@NotNull
 	@Override
 	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
-		tripHeadsign = CleanUtils.keepToAndRemoveVia(tripHeadsign);
-		tripHeadsign = CleanUtils.fixMcXCase(tripHeadsign);
-		tripHeadsign = CleanUtils.cleanBounds(tripHeadsign);
-		tripHeadsign = CleanUtils.cleanSlashes(tripHeadsign);
-		tripHeadsign = CleanUtils.cleanLabel(tripHeadsign);
-		return tripHeadsign; // DO NOT CLEAN, USED TO IDENTIFY TRIP IN REAL TIME API // <= TODO REALLY ???
-	}
-
-	@Override
-	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
-		throw new MTLog.Fatal("Can't merge headsign for trips %s and %s!", mTrip, mTripToMerge);
+		tripHeadsign = CleanUtils.removeVia(tripHeadsign);
+		return OttawaOCTranspoProviderCommons.cleanTripHeadsign(tripHeadsign);
 	}
 
 	private static final Pattern ENDS_WITH_DIRECTION = Pattern.compile("(" //
@@ -198,15 +139,21 @@ public class OttawaOCTranspoTrainAgencyTools extends DefaultAgencyTools {
 			+ "north / nord|south / sud|east / est|west / ouest" //
 			+ ")", Pattern.CASE_INSENSITIVE);
 
-	private static final Pattern O_TRAIN_ = CleanUtils.cleanWords("o-train");
+	private static final Pattern O_TRAIN_ = CleanUtils.cleanWords("o-train", "o train");
 	private static final String O_TRAIN_REPLACEMENT = CleanUtils.cleanWordsReplacement(EMPTY);
+
+	private String[] getIgnoredWords() {
+		return new String[]{
+				"TOH"
+		};
+	}
 
 	@NotNull
 	@Override
 	public String cleanStopName(@NotNull String gStopName) {
 		gStopName = O_TRAIN_.matcher(gStopName).replaceAll(O_TRAIN_REPLACEMENT);
 		gStopName = ENDS_WITH_DIRECTION.matcher(gStopName).replaceAll(EMPTY);
-		gStopName = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, gStopName);
+		gStopName = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, gStopName, getIgnoredWords());
 		gStopName = CleanUtils.fixMcXCase(gStopName);
 		gStopName = CleanUtils.cleanBounds(gStopName);
 		gStopName = CleanUtils.cleanNumbers(gStopName);
@@ -229,15 +176,15 @@ public class OttawaOCTranspoTrainAgencyTools extends DefaultAgencyTools {
 	@Override
 	public int getStopId(@NotNull GStop gStop) {
 		String stopCode = getStopCode(gStop);
-		if (stopCode.length() > 0 && Utils.isDigitsOnly(stopCode)) {
+		if (stopCode.length() > 0 && CharUtils.isDigitsOnly(stopCode)) {
 			return Integer.parseInt(stopCode); // using stop code as stop ID
 		}
 		//noinspection deprecation
 		final String stopId1 = gStop.getStopId();
-		Matcher matcher = DIGITS.matcher(stopId1);
+		final Matcher matcher = DIGITS.matcher(stopId1);
 		if (matcher.find()) {
-			int digits = Integer.parseInt(matcher.group());
-			int stopId;
+			final int digits = Integer.parseInt(matcher.group());
+			final int stopId;
 			if (stopId1.startsWith(EE)) {
 				stopId = 100_000;
 			} else if (stopId1.startsWith(EO)) {
